0e5534dee868b0ee80e060677b615e88f347381c
iiNIGHTKINGII <50060233+iiNIGHTKINGII@users.noreply.github.com>
2019-05-02 19:06:33 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,7 +27,7 @@ const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 const sql = require("sqlite");
 const dateFormat = require('dateformat'); 
 const pretty = require('pretty-ms') 
-const prefix = 'a';
+const prefix = '!';
 var table = require('table').table
 var ti={}  
 ,spee={}
@@ -46,7 +46,7 @@ client.on('ready', function(){
             j = -1;
         }
         i = i+j;
-        client.user.setGame(setGame[i],`http://www.twitch.tv/barontube`);
+        client.user.setGame(setGame[i],`!help`);
     }, ms);
     console.log(` ????????? |> Name: ${client.user.username}`);
  console.log(` ????????? |> Servers: ${client.guilds.size}`);
